cde8fc9dfb67a5e32d7e8fb6ab083cc21745817f
Tania-frontend <tatyanagroshevaya@gmail.com>
2021-04-16 18:31:52 +0300

    added new file

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,62 @@
 $(document).ready(function() {
+ var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 'auto',
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-//Login modal
-$('.popup-link').on('click', function (e){
-  $('.popup-map').fadeIn();
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+            delay: 60000,
+            disableOnInteraction: false
+  },
+ })
+
+ $('.navigation').on('click', '.navigation__menu_mobile', function(){
+$('.navigation').toggleClass('menu-opened');
 
 });
 
 
-$('.popup__close').on('click', function (e){
-  $('.popup-map').fadeOut();
+//Login modal
+$('.js-login').on('click', function (e){
+  $('.modal--login').fadeIn();
+
+});
+
+
+$('.modal__close').on('click', function (e){
+  $('.modal--login').fadeOut();
 
 
 });
 
 $(document).on('mouseup', function (event){
-  const popupMap = $('.popup__window');
-  if(!popupMap.is(event.target) && popupMap.has(event.target).length === 0){
-      $('.popup').fadeOut();
+  const loginModal = $('.modal__window');
+  if(!loginModal.is(event.target) && loginModal.has(event.target).length === 0){
+      $('.modal').fadeOut();
   }
 })
 
 });
 
+
+
+$(document).on('mouseup', function (event){
+  const menuMobile = $('.menu-opened .navigation__menu');
+  if(!menuMobile.is(event.target) && menuMobile.has(event.target).length === 0){
+      $('.menu-opened .navigation__menu').fadeOut();
+  }
+});
 
 
 
