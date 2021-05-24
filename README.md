642454b1a7dd2301c779163bf0c495ade44b09fe
Tania-frontend <tatyanagroshevaya@gmail.com>
2021-05-24 20:07:06 +0300

    added new file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# viani
+# starelektro
--- gulpfile.js
+++ gulpfile.js
@@ -53,9 +53,9 @@ function scripts() {
 }
 
 function styles() {
-	return src('app/scss/main.scss') // Выбираем источник: "app/sass/main.sass"
+	return src('app/scss/all.scss') // Выбираем источник: "app/sass/main.sass"
 	.pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
-	.pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+	.pipe(concat('all.css')) // Конкатенируем в файл app.min.js
 	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
 	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
 	.pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
